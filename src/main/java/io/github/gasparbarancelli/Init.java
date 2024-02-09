d8f5061c86c7c650e1103088f39add7d65ae221d
gasparbarancelli <gasparbarancelli@gmail.com>
2024-02-09 12:51:12 -0300

    teste

--- src/main/java/io/github/gasparbarancelli/Init.java
+++ src/main/java/io/github/gasparbarancelli/Init.java
@@ -4,7 +4,10 @@ package io.github.gasparbarancelli;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.LockModeType;
 import jakarta.transaction.Transactional;
+
+import java.util.Objects;
 
 @ApplicationScoped
 public class Init {
@@ -14,13 +17,20 @@ public class Init {
 
     @Transactional
     public void incluirClientes() {
-        entityManager.createNativeQuery("""
-                REPLACE INTO CLIENTE (ID, LIMITE, SALDO)
-                VALUES (1, 100000, 0),
-                       (2, 80000, 0),
-                       (3, 1000000, 0),
-                       (4, 10000000, 0),
-                       (5, 500000, 0);
-        """).executeUpdate();
+        try {
+            var cliente = entityManager.find(Cliente.class, 1, LockModeType.PESSIMISTIC_READ);
+            if (Objects.isNull(cliente)) {
+                entityManager.createNativeQuery("""
+                        REPLACE INTO CLIENTE (ID, LIMITE, SALDO)
+                        VALUES (1, 100000, 0),
+                               (2, 80000, 0),
+                               (3, 1000000, 0),
+                               (4, 10000000, 0),
+                               (5, 500000, 0);
+            """).executeUpdate();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
